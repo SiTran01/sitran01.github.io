cbd87eb993e1f90348dcd34c037185adc9efe1df
SiTran01 <quocsi15@gmail.com>
2025-07-22 21:37:40 +0700

    adjust UI

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@ const buttons = document.querySelectorAll('#section2 .button');
 const descBox = document.getElementById('service-description');
 const square = document.getElementById('square');
 const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+const emailLink = document.getElementById('email-link');
 let currentHoveredButton = null;
 let currentVisible = null;
 let descTimeout = null;
+
+if (isMobile) {
+    emailLink.href = "mailto:quocsi15@gmail.com";
+  } else {
+    emailLink.href = "https://mail.google.com/mail/?view=cm&fs=1&to=quocsi15@gmail.com";
+  }
 
 function updateDescription(button) {
   const img = button.dataset.img || '';
